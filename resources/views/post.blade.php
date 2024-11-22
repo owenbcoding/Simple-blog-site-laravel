2299fdea5e278a0b158323d2d4f217c2c3a43d8a
owenbcoding <eoghankb@gmail.com>
2024-11-22 13:50:58 +0000

    Created variable for post on show post page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,10 +12,11 @@
 
 <body>
     <article>
-        <h1>First Post</h1>
+        <!-- <h1>First Post</h1>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime praesentium reiciendis officia esse ratione rerum voluptatem nam tempore fugit sint qui porro amet veniam animi repudiandae nobis non, aut vero! Quod possimus quasi labore pariatur aspernatur earum iure architecto, excepturi, laudantium exercitationem saepe officiis nam. Esse, qui deserunt nihil aperiam nisi temporibus cupiditate, consectetur doloribus nulla, ratione quasi quaerat. Beatae debitis provident impedit repellendus a accusamus in. Nostrum iure inventore enim non soluta aliquam quibusdam dolorem autem cum adipisci! Quis impedit ad reprehenderit quidem officiis ducimus aliquid. Iure odit totam, est consectetur natus ut nostrum aliquid doloremque, magnam adipisci ad!
-        </p>
+        </p> -->
+        <?= $post; ?>
     </article>
     <a href="/">Go Back</a>
 </body>
