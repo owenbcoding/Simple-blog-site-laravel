d04ba394c5da503cf273c4d2165ffb4798bc1b2c
owenbcoding <eoghankb@gmail.com>
2024-11-22 13:35:07 +0000

    created main post page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+    <title>Blog</title>
+    <!-- <script src="app.js"></script> -->
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime praesentium reiciendis officia esse ratione rerum voluptatem nam tempore fugit sint qui porro amet veniam animi repudiandae nobis non, aut vero! Quod possimus quasi labore pariatur aspernatur earum iure architecto, excepturi, laudantium exercitationem saepe officiis nam. Esse, qui deserunt nihil aperiam nisi temporibus cupiditate, consectetur doloribus nulla, ratione quasi quaerat. Beatae debitis provident impedit repellendus a accusamus in. Nostrum iure inventore enim non soluta aliquam quibusdam dolorem autem cum adipisci! Quis impedit ad reprehenderit quidem officiis ducimus aliquid. Iure odit totam, est consectetur natus ut nostrum aliquid doloremque, magnam adipisci ad!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime praesentium reiciendis officia esse ratione rerum voluptatem nam tempore fugit sint qui porro amet veniam animi repudiandae nobis non, aut vero! Quod possimus quasi labore pariatur aspernatur earum iure architecto, excepturi, laudantium exercitationem saepe officiis nam. Esse, qui deserunt nihil aperiam nisi temporibus cupiditate, consectetur doloribus nulla, ratione quasi quaerat. Beatae debitis provident impedit repellendus a accusamus in. Nostrum iure inventore enim non soluta aliquam quibusdam dolorem autem cum adipisci! Quis impedit ad reprehenderit quidem officiis ducimus aliquid. Iure odit totam, est consectetur natus ut nostrum aliquid doloremque, magnam adipisci ad!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime praesentium reiciendis officia esse ratione rerum voluptatem nam tempore fugit sint qui porro amet veniam animi repudiandae nobis non, aut vero! Quod possimus quasi labore pariatur aspernatur earum iure architecto, excepturi, laudantium exercitationem saepe officiis nam. Esse, qui deserunt nihil aperiam nisi temporibus cupiditate, consectetur doloribus nulla, ratione quasi quaerat. Beatae debitis provident impedit repellendus a accusamus in. Nostrum iure inventore enim non soluta aliquam quibusdam dolorem autem cum adipisci! Quis impedit ad reprehenderit quidem officiis ducimus aliquid. Iure odit totam, est consectetur natus ut nostrum aliquid doloremque, magnam adipisci ad!
+        </p>
+    </article>
+</body>
+
+</html>
