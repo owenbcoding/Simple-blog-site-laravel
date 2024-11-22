b2d4254e88f94268c233e4b87044cfffcb5843c7
owenbcoding <eoghankb@gmail.com>
2024-11-22 13:51:20 +0000

    Changed project title

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/app.css">
     <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-    <title>Blog</title>
+    <title>A-Simple-Blog</title>
     <!-- <script src="app.js"></script> -->
 </head>
 
